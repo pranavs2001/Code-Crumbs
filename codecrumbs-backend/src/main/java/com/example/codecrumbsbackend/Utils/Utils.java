6532bc21decf60592f91a7f02ff84b362e906d2e
Vishal <vishal.anantharaman@gmail.com>
2021-03-22 16:43:01 -0700

    Added Set field function

--- codecrumbs-backend/src/main/java/com/example/codecrumbsbackend/Utils/Utils.java
+++ codecrumbs-backend/src/main/java/com/example/codecrumbsbackend/Utils/Utils.java
@@ -4,4 +4,7 @@ public class Utils {
 
     public static final String USERS = "users";
     public static final String PROJECTS = "projects";
+    public static final String GITHUB_APP_AUTH_LINK = "https://github.com/login/oauth/authorize";
+    public static final String GITHUB_APP_AUTH_ACCESS_TOKEN_LINK = "https://github.com/login/oauth/access_token";
+    public static final String GITHUB_APP_AUTH_SCOPE = "scope=repo";
 }
