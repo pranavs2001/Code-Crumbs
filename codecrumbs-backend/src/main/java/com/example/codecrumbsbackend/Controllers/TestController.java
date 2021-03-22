1b5851fd9f33b4690f9efcdb0abd74b7899abbc7
Vishal <vishal.anantharaman@gmail.com>
2021-03-22 15:01:00 -0700

    monkey stuff

--- codecrumbs-backend/src/main/java/com/example/codecrumbsbackend/Controllers/TestController.java
+++ codecrumbs-backend/src/main/java/com/example/codecrumbsbackend/Controllers/TestController.java
@@ -1,7 +1,19 @@
 package com.example.codecrumbsbackend.Controllers;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.StringJoiner;
 
 import javax.print.attribute.HashAttributeSet;
 
@@ -11,6 +23,8 @@ import com.example.codecrumbsbackend.Repositories.UserRepository;
 
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+
+import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -44,6 +58,52 @@ public class TestController {
         HashMap<String, String> map = new HashMap<>();
         map.put("Status", "Success");
         map.put("URL", githubAuthUrl);
+        return map;
+    }
+
+    @GetMapping("/Github-auth-callback")
+    public Map<String, String> getGithubAuthCallback(@RequestParam String code) {
+        String urlBase = System.getenv("GITHUB_APP_AUTH_ACCESS_TOKEN_LINK");
+        HashMap<String, String> map = new HashMap<>();
+        try {
+            URL url = new URL(System.getenv(urlBase));
+            HttpURLConnection post = (HttpURLConnection)url.openConnection();
+            post.setRequestMethod("POST");
+            post.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+            post.setRequestProperty("Accept", "application/json");
+            post.setDoOutput(true);
+
+            HashMap<String, String> urlParams = new HashMap<>();
+            urlParams.put("client_id", System.getenv("GITHUB_APP_CLIENT_ID"));
+            urlParams.put("client_secret", System.getenv("GITHUB_APP_CLIENT_SECRET"));
+            urlParams.put("code", code);
+
+            StringJoiner s = new StringJoiner("&");
+            for(Map.Entry<String, String> temp : urlParams.entrySet()) {
+                s.add(URLEncoder.encode(temp.getKey(), "UTF-8") + "=" + URLEncoder.encode(temp.getValue(), "UTF-8"));
+            }
+
+            byte[] output = s.toString().getBytes(StandardCharsets.UTF_8);
+            post.setFixedLengthStreamingMode(output.length);
+            post.connect();
+
+            try(OutputStream os = post.getOutputStream()) {
+                os.write(output);
+            }
+
+            try(BufferedReader bf = new BufferedReader(new InputStreamReader(post.getInputStream(), "utf-8"))) {
+                StringBuilder response = new StringBuilder();
+                String responseLine = null;
+                while ((responseLine = bf.readLine()) != null) {
+                    response.append(responseLine.trim());
+                }
+                String finalResponse = response.toString();
+                String[] responseComponents = finalResponse.split("&");
+                String accessToken = responseComponents[0].split("=")[1];
+            }
+        } catch (Exception e) {
+            map.put("Status", "Error: " + e.getLocalizedMessage());
+        }
         return map;
     }
 }
