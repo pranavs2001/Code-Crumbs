0e3b8b1915ecda7482729b97fc5883cf0582ec9c
Vishal <vishal.anantharaman@gmail.com>
2021-03-23 11:21:16 -0700

    Yay we have merge errors

--- codecrumbs-backend/src/main/java/com/example/codecrumbsbackend/Controllers/GithubController.java
+++ codecrumbs-backend/src/main/java/com/example/codecrumbsbackend/Controllers/GithubController.java
@@ -13,7 +13,9 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.StringJoiner;
 
@@ -24,9 +26,15 @@ import com.example.codecrumbsbackend.Models.User;
 import com.example.codecrumbsbackend.Repositories.FirebaseService;
 import com.example.codecrumbsbackend.Repositories.UserRepository;
 import com.example.codecrumbsbackend.Utils.Utils;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.api.client.json.Json;
 import com.google.common.base.Optional;
 
 import lombok.Getter;
+import lombok.val;
 import lombok.extern.slf4j.Slf4j;
 
 import org.apache.tomcat.util.json.JSONParser;
@@ -46,12 +54,11 @@ public class GithubController {
     //The client id and secret are environment variables for security purposes, found in app.yaml
     @GetMapping("/Github-auth-link")
     public Map<String, String> getGithubAuthLink() {
-        String githubAuthUrl = Utils.GITHUB_APP_AUTH_LINK + "?";
-        githubAuthUrl += Utils.GITHUB_APP_AUTH_SCOPE + "&";
-        githubAuthUrl += "client_id=" + System.getenv("GITHUB_APP_CLIENT_ID");
+        String[] urlList = {Utils.GITHUB_APP_AUTH_LINK + "?", Utils.GITHUB_APP_AUTH_SCOPE + "&", Utils.GITHUB_API_CLIENT_ID_KEY + "=" , System.getenv("GITHUB_APP_CLIENT_ID")};
+        String githubAuthUrl = urlFormatter(urlList);
         HashMap<String, String> map = new HashMap<>();
-        map.put("Status", "Success");
-        map.put("URL", githubAuthUrl);
+        map.put(Utils.STATUS, Utils.SUCCESS);
+        map.put(Utils.URL, githubAuthUrl);
         return map;
     }
 
@@ -60,23 +67,22 @@ public class GithubController {
         String urlBase = Utils.GITHUB_APP_AUTH_ACCESS_TOKEN_LINK;
         HashMap<String, String> map = new HashMap<>();
         try {
-            HashMap<String, String> urlParams = new HashMap<>();
-            urlParams.put("client_id", System.getenv("GITHUB_APP_CLIENT_ID"));
-            urlParams.put("client_secret", System.getenv("GITHUB_APP_CLIENT_SECRET"));
-            urlParams.put("code", code);
-
+            String[] keys = {Utils.GITHUB_API_CLIENT_ID_KEY, Utils.GITHUB_API_CLIENT_SECRET_KEY, Utils.GITHUB_API_CODE_KEY};
+            String[] values = {System.getenv("GITHUB_APP_CLIENT_ID"), System.getenv("GITHUB_APP_CLIENT_SECRET"), code};
+            Map<String, String> urlParams = parameterMap(keys, values);
+            
             String finalResponse = postHelper(urlBase, urlParams);
             
-            if(finalResponse.substring(0, 6) == "Error") {
-                map.put("Status", finalResponse);
+            if(errorCheck(finalResponse)) {
+                map.put(Utils.STATUS, finalResponse);
             } else {
                 String[] responseComponents = finalResponse.split("&");
                 String accessToken = responseComponents[0].split("=")[1];
-                map.put("Status", "Success");
-                map.put("AccessToken", accessToken);
+                map.put(Utils.STATUS, Utils.SUCCESS);
+                map.put(Utils.ACCESS_TOKEN_KEY, accessToken);
             }
         } catch (Exception e) {
-            map.put("Status", "Error: " + e.getLocalizedMessage());
+            map.put(Utils.STATUS, "Error: " + e.getLocalizedMessage());
         }
         return map;
     }
@@ -84,70 +90,150 @@ public class GithubController {
     @PostMapping("/Github-access-token-response")
     public Map<String, String> postAccessToken(@RequestParam Map<String, String> allParams) {
         String userId = allParams.get("userId");
-        String accessToken = allParams.get("access_token");
+        String accessToken = allParams.get(Utils.ACCESS_TOKEN);
 
         Map<String, String> temp = new HashMap<>();
-        temp.put("access_token", accessToken);
+        temp.put(Utils.ACCESS_TOKEN, accessToken);
         Map<String, Object> finalParams = new HashMap<>(temp);
 
         userRepository.setUserField(userId, finalParams);
 
         HashMap<String, String> returnMap = new HashMap<>();
-        returnMap.put("Status", "Success");
+        returnMap.put(Utils.STATUS, Utils.SUCCESS);
         return returnMap;
     }
 
-    @GetMapping("/Github-get-repos")
-    public Map<String, String> getUserRepos(@RequestParam String userId) {
-        HashMap<String, String> returnMap = new HashMap<>();
+    @GetMapping("/Github-get-all-repos")
+    public ObjectNode getUserRepos(@RequestParam String userId) {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode objNode = mapper.createObjectNode();
         
         String accessToken = userRepository.getUserInfo(userId).getGithubAccessToken();
-        String urlResource = Utils.GITHUB_API_BASE_URL + Utils.GITHUB_API_REPO_ENDPOINT;
+        String[] urlElems = {Utils.GITHUB_API_BASE_URL, Utils.GITHUB_API_REPO_ENDPOINT};
+        String urlResource = urlFormatter(urlElems);
 
-        Map<String, String> paramMap = new HashMap<>();
-        paramMap.put("access_token", accessToken);
-        paramMap.put("visibility", "all");
-        paramMap.put("sort", "full_name");
-        paramMap.put("per_page", "100");
+        String[] keys = {Utils.ACCESS_TOKEN, Utils.VISIBILITY, Utils.SORT, Utils.PER_PAGE};
+        String[] values = {accessToken, Utils.ALL, Utils.FULL_NAME, Utils.HUNDRED};
+        Map<String, String> paramMap = parameterMap(keys, values);
+
+        String finalResponse = postHelper(urlResource, paramMap);
+
+        if(errorCheck(finalResponse)) {
+            objNode.put(Utils.STATUS, finalResponse);
+        } else {
+            JsonArray jsonobj = JsonParser.parseString(finalResponse).getAsJsonArray();
+            int i;
+            for(i = 0; i < jsonobj.size(); i++) {
+                ObjectNode tempJson = mapper.createObjectNode();
+                JsonObject temp = jsonobj.get(i).getAsJsonObject();
+                JsonObject owner = temp.get("owner").getAsJsonObject();
+                tempJson.put(Utils.OWNER, owner.get(Utils.LOGIN).getAsString());
+                tempJson.put(Utils.REPO_NAME, temp.get(Utils.REPO_NAME).getAsString());
+    
+                String[] tempUrlElems = {Utils.GITHUB_API_BASE_URL + "/", Utils.GITHUB_API_COLLABORATORS_ENDPOINT.split("/")[0] + "/", tempJson.get(Utils.OWNER) + "/", tempJson.get(Utils.REPO_NAME) + "/", Utils.GITHUB_API_COLLABORATORS_ENDPOINT.split("/")[1]};
+                urlResource = urlFormatter(tempUrlElems);
+                
+                String[] keysTemp = {Utils.PER_PAGE, Utils.ACCESS_TOKEN};
+                String[] valuesTemp = {Utils.HUNDRED, accessToken};
+                paramMap.clear();
+                paramMap = parameterMap(keysTemp, valuesTemp);
+
+                finalResponse = postHelper(urlResource, paramMap);
+    
+                if(errorCheck(finalResponse)) {
+                    continue;
+                }
+
+                JsonArray jsonCollaborators = JsonParser.parseString(finalResponse).getAsJsonArray();
+                int j;
+                ObjectNode collaboratorsArr = mapper.createObjectNode();
+                for(j = 0; j < jsonCollaborators.size(); j++) {
+                    ObjectNode tempCollaboratorsJson = mapper.createObjectNode();
+                    JsonObject tempCollaborator = jsonCollaborators.get(i).getAsJsonObject();
+                    tempCollaboratorsJson.put(Utils.LOGIN, tempCollaborator.get(Utils.LOGIN).getAsString());
+                    
+                    collaboratorsArr.set(Integer.toString(j), tempCollaboratorsJson);
+                }
+                tempJson.set(Utils.COLLABORATORS, collaboratorsArr);
+                tempJson.put(Utils.NUM_COLLABORATORS, Integer.toString(j));
+    
+                objNode.set(Integer.toString(i), tempJson);
+            }
+            objNode.put(Utils.STATUS, Utils.SUCCESS);
+            objNode.put(Utils.NUM_REPOS, Integer.toString(i));
+        }
+        
+        return objNode;
+    }
+
+    @GetMapping("/Github-get-repo")
+    public ObjectNode getUserRepo(@RequestParam String userId, @RequestParam String owner, 
+                                    @RequestParam String repo) {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode objNode = mapper.createObjectNode();
+        objNode.put(Utils.OWNER, owner);
+        objNode.put(Utils.REPO_NAME, repo);
+        String accessToken = userRepository.getUserInfo(userId).getGithubAccessToken();
+        String[] urlElems = {Utils.GITHUB_API_BASE_URL + "/", Utils.GITHUB_API_COLLABORATORS_ENDPOINT.split("/")[0] + "/", owner + "/", repo + "/", Utils.GITHUB_API_COLLABORATORS_ENDPOINT.split("/")[1]};
+        String urlResource = urlFormatter(urlElems);
+
+        String[] keys = {Utils.ACCESS_TOKEN, Utils.PER_PAGE};
+        String[] values = {accessToken, Utils.HUNDRED};
+        Map<String, String> paramMap = parameterMap(keys, values);
 
         String finalResponse = postHelper(urlResource, paramMap);
         
-        if(finalResponse.substring(0, 6) == "Error") {
-            returnMap.put("Status", finalResponse);
+        if(errorCheck(finalResponse)) {
+            objNode.put(Utils.STATUS, finalResponse);
         } else {
-            returnMap.put("Status", "Success");
-            returnMap.put("Repos", finalResponse);
+            JsonArray jsonCollaborators = JsonParser.parseString(finalResponse).getAsJsonArray();
+            int i;
+            ObjectNode collaboratorsArr = mapper.createObjectNode();
+            for(i = 0; i < jsonCollaborators.size(); i++) {
+                ObjectNode tempCollaboratorsJson = mapper.createObjectNode();
+                JsonObject tempCollaborator = jsonCollaborators.get(i).getAsJsonObject();
+                tempCollaboratorsJson.put(Utils.LOGIN, tempCollaborator.get(Utils.LOGIN).getAsString());
+                collaboratorsArr.set(Integer.toString(i), tempCollaboratorsJson);
+            }
+            objNode.set(Utils.COLLABORATORS, collaboratorsArr);
+            objNode.put(Utils.NUM_COLLABORATORS, Integer.toString(i));
         }
-
-        return returnMap;
+        return objNode;
     }
-
+    
     @GetMapping("/Github-get-commits")
-    public Map<String, String> getUserCommits(@RequestParam String userId, @RequestParam String owner, 
+    public ObjectNode getUserCommits(@RequestParam String userId, @RequestParam String owner, 
                                                 @RequestParam String repo, @RequestParam String since) {
-        HashMap<String, String> returnMap = new HashMap<>();
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode objNode = mapper.createObjectNode();
 
         String accessToken = userRepository.getUserInfo(userId).getGithubAccessToken();
-        String urlResource = Utils.GITHUB_API_BASE_URL + "/";
-        urlResource += Utils.GITHUB_API_COMMIT_ENDPOINT.split("/")[0];
-        urlResource += owner;
-        urlResource += repo;
-        urlResource += Utils.GITHUB_API_COMMIT_ENDPOINT.split("/")[1];
+        String[] urlElems = {Utils.GITHUB_API_BASE_URL + "/", Utils.GITHUB_API_COMMIT_ENDPOINT.split("/")[0] + "/", owner + "/", repo + "/", Utils.GITHUB_API_COMMIT_ENDPOINT.split("/")[1]};
+        String urlResource = urlFormatter(urlElems);
 
-        Map<String, String> paramMap = new HashMap<>();
-        paramMap.put("access_token", accessToken);
-        paramMap.put("since", since);
+        String[] keys = {Utils.ACCESS_TOKEN, Utils.SINCE};
+        String[] values = {accessToken, since};
+        Map<String, String> paramMap = parameterMap(keys, values);
 
         String finalResponse = postHelper(urlResource, paramMap);
 
-        if(finalResponse.substring(0, 6) == "Error") {
-            returnMap.put("Status", finalResponse);
+        JsonArray jsonobj = JsonParser.parseString(finalResponse).getAsJsonArray();
+
+        if(errorCheck(finalResponse)) {
+            objNode.put(Utils.STATUS, finalResponse);
         } else {
-            returnMap.put("Status", "Success");
-            returnMap.put("Repos", finalResponse);
+            int i;
+            for(i = 0; i < jsonobj.size(); i++) {
+                JsonObject temp = jsonobj.get(i).getAsJsonObject();
+                JsonObject commit = temp.get("commit").getAsJsonObject();
+                JsonObject author = commit.get("author").getAsJsonObject();
+                objNode.set(Integer.toString(i), mapper.readTree(author.toString()));
+            }
+            objNode.put(Utils.STATUS, Utils.SUCCESS);
+            objNode.put(Utils.NUM_COMMITS, Integer.toString(i));
         }
 
-        return returnMap;
+        return objNode;
     }
 
 
@@ -183,7 +269,27 @@ public class GithubController {
                 return finalResponse;
             }        
         } catch (Exception e) {
-            return "Error" + e.getLocalizedMessage();
+            return Utils.ERROR + e.getLocalizedMessage();
         }
+    }
+
+    private String urlFormatter(String[] elems) {
+        String temp = "";
+        for(String elem : elems) {
+            temp += elem;
+        }
+        return temp;
+    }
+
+    private Boolean errorCheck(String s) {
+        return s.substring(0, 6) == Utils.ERROR;
+    }
+
+    private Map<String, String> parameterMap(String[] keys, String[] values) {
+        Map<String, String> temp = new HashMap<>();
+        for(int i = 0; i < keys.length; i++) {
+            temp.put(keys[i], values[i]);
+        }
+        return temp;
     }
 }
--- codecrumbs-backend/src/main/java/com/example/codecrumbsbackend/Utils/Utils.java
+++ codecrumbs-backend/src/main/java/com/example/codecrumbsbackend/Utils/Utils.java
@@ -8,11 +8,39 @@ public class Utils {
     public static final String NUMBER_OF_SEARCHES = "numberOfSearches";
     public static final String TIME_ACCESSED = "timeAccessed";
     public static final String COMMENTS = "comments";
-
+    public static final String STATUS = "Status";
+    public static final String SUCCESS = "Success";
+    public static final String ACCESS_TOKEN = "access_token";
+    public static final String VISIBILITY = "visibility";
+    public static final String SORT = "sort";
+    public static final String PER_PAGE = "per_page";
+    public static final String ACCESS_TOKEN_KEY = "AccessToken";
+    public static final String URL = "URL";
+    public static final String REPOS = "Repos";
+    public static final String SINCE = "since";
+    public static final String NUM_COMMITS = "NumCommits";
+    public static final String NUM_REPOS = "NumRepos";
+    public static final String NUM_COLLABORATORS = "NumCollaborators";
+    public static final String LOGIN = "login";
+    public static final String OWNER = "owner";
+    public static final String REPO_NAME = "name";
+    public static final String COLLABORATORS = "collaborators";
+    public static final String ERROR = "Error";
+    public static final String ALL = "all";
+    public static final String FULL_NAME = "full_name";
+    public static final String HUNDRED = "100";
+        
     public static final String GITHUB_APP_AUTH_LINK = "https://github.com/login/oauth/authorize";
     public static final String GITHUB_APP_AUTH_ACCESS_TOKEN_LINK = "https://github.com/login/oauth/access_token";
     public static final String GITHUB_APP_AUTH_SCOPE = "scope=repo";
     public static final String GITHUB_API_BASE_URL = "https://api.github.com";
     public static final String GITHUB_API_REPO_ENDPOINT = "/user/repos";
+    public static final String GITHUB_API_SINGLE_REPO_ENDPOINT = "/repos";
     public static final String GITHUB_API_COMMIT_ENDPOINT = "/repos/commits";
+    public static final String GITHUB_API_COLLABORATORS_ENDPOINT = "/repos/collaborators";
+    public static final String GITHUB_API_CLIENT_ID_KEY = "client_id";
+    public static final String GITHUB_API_CLIENT_SECRET_KEY = "client_secret";
+    public static final String GITHUB_API_CODE_KEY = "code";
+
+    
 }
