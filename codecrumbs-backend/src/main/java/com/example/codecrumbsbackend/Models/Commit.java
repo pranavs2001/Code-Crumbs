3483655a9cd1d47304a6e96e744bf4a3f000c171
Vishal <vishal.anantharaman@gmail.com>
2021-03-24 12:25:09 -0700

    Object update

---
+++ codecrumbs-backend/src/main/java/com/example/codecrumbsbackend/Models/Commit.java
@@ -0,0 +1,15 @@
+package com.example.codecrumbsbackend.Models;
+
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Commit {
+    private String userId;
+    private String owner;
+    private String repo;
+    private String since;
+}
